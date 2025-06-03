9f994f4bb63d17191cec433ca3b76b9421ba0042
jeanmbiz <jeanmbiz@hotmail.com>
2025-06-02 23:19:36 -0300

    feat: add truck plate vo

---
+++ src/domain/value-objects/truck-plate.ts
@@ -0,0 +1,23 @@
+export class TruckPlate {
+  private readonly value: string;
+
+  constructor(plate: string) {
+    if (!this.isValid(plate)) {
+      throw new Error('plate do caminhão inválida');
+    }
+    this.value = plate.toUpperCase();
+  }
+
+  getValue(): string {
+    return this.value;
+  }
+
+  private isValid(plate: string): boolean {
+    const platePattern = /^[A-Z]{3}-\d{4}$/;
+    return platePattern.test(plate.toUpperCase());
+  }
+
+  equals(other: TruckPlate): boolean {
+    return this.value === other.value;
+  }
+}
