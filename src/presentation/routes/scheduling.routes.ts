ef84ffec52ea84b279afea22326879117b9c0b49
jeanmbiz <jeanmbiz@hotmail.com>
2025-06-03 07:41:06 -0300

    feat: add route get

--- src/presentation/routes/scheduling.routes.ts
+++ src/presentation/routes/scheduling.routes.ts
@@ -5,22 +5,31 @@ import { CreateScheduleUseCase } from '../../application/use-cases/create-schedu
 import { SchedulingController } from '../controllers/scheduling.controller';
 import { ChangeStatusSchedulingUseCase } from '../../application/use-cases/change-status-schedule.use-case';
 import { ChangeStatusValidation } from '../middlewares/change-status-validation.middleware';
+import { ListSchedulesController } from '../controllers/list-schedules.controller';
+import { createScheduleValidation } from '../middlewares/create-schedule-validation.middleware';
+import { ListSchedulesUseCase } from '../../application/use-cases/list-schedule.use-case';
 
 const router = Router();
 
 const schedulingRepository = new InMemorySchedulingRepository();
 const createScheduleUseCase = new CreateScheduleUseCase(schedulingRepository);
 const changeStatusSchedulingUseCase = new ChangeStatusSchedulingUseCase(schedulingRepository)
+const listSchedulesUseCase = new ListSchedulesUseCase(schedulingRepository)
 
 const schedulingController = new SchedulingController(createScheduleUseCase, changeStatusSchedulingUseCase);
+const listSchedulesController = new ListSchedulesController(listSchedulesUseCase)
 
-router.post('/agendamentos', (req: Request, res: Response) =>
+router.post('/agendamentos', createScheduleValidation, (req: Request, res: Response) =>
   schedulingController.createSchedule(req, res)
 );
 
-
 router.patch('/agendamentos/:id/status', ChangeStatusValidation, (req: Request, res: Response) =>
   schedulingController.changeStatus(req, res)
+);
+
+router.get(
+  '/agendamentos',
+  (req, res) => listSchedulesController.handle(req, res)
 );
 
 export { router as schedulingRoutes };
