53a2f80561f17c4ceedb97a79ae48e3a3126adbd
jeanmbiz <jeanmbiz@hotmail.com>
2025-06-03 07:54:22 -0300

    refactor: add errorHandler and separation routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
-import { createScheduleValidation } from './presentation/middlewares/create-schedule-validation.middleware';
-import { schedulingRoutes } from './presentation/routes/scheduling.routes';
+import { createScheduleRoutes } from './presentation/routes/create-schedule.routes';
+import { changeStatusScheduleRoutes } from './presentation/routes/change-status-schedule.routes';
+import { listSchedulesRoutes } from './presentation/routes/list-schedules.routes';
+import { errorHandler } from './presentation/middlewares/error-handler.middleware';
 
 const app = express();
 
 app.use(express.json());
-app.use('/api', createScheduleValidation)
-app.use('/api', schedulingRoutes);
+
+app.use('/api', createScheduleRoutes);
+app.use('/api', changeStatusScheduleRoutes);
+app.use('/api', listSchedulesRoutes);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
