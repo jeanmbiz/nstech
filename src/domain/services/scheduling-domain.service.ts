f06cbe11cd86bd5a2f5049e7c2b225fc3bb3bead
jeanmbiz <jeanmbiz@hotmail.com>
2025-06-02 23:20:58 -0300

    feat: add scheduling domain service

---
+++ src/domain/services/scheduling-domain.service.ts
@@ -0,0 +1,38 @@
+import { Scheduling } from '../entities/scheduling';
+
+export class SchedulingDomainService {
+  static validateConflicts(
+    newScheduling: Scheduling,
+    existingScheduling: Scheduling[]
+  ): void {
+    this.validateTimeConflict(newScheduling, existingScheduling);
+    this.validateDriverConflict(newScheduling, existingScheduling);
+  }
+
+  private static validateTimeConflict(
+    newScheduling: Scheduling,
+    existingScheduling: Scheduling[]
+  ): void {
+    const conflict = existingScheduling.some(scheduling =>
+      newScheduling.hasConflict(scheduling)
+    );
+
+    if (conflict) {
+      throw new Error('Conflito de agendamento');
+    }
+  }
+
+  private static validateDriverConflict(
+    newScheduling: Scheduling,
+    existingScheduling: Scheduling[]
+  ): void {
+    const driverHasConflict = existingScheduling.some(scheduling =>
+      scheduling.motoristaCpf.equals(newScheduling.motoristaCpf) &&
+      scheduling.driverHasConflictingStatus()
+    );
+
+    if (driverHasConflict) {
+      throw new Error('Conflito de agendamento');
+    }
+  }
+}
