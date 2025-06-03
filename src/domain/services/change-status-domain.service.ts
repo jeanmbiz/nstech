28b3b8d1c87439fec77383e20d8fbaca8f2767bf
jeanmbiz <jeanmbiz@hotmail.com>
2025-06-02 23:28:44 -0300

    feat: add ChangeStatusDomainService

---
+++ src/domain/services/change-status-domain.service.ts
@@ -0,0 +1,21 @@
+import { Scheduling } from "../entities/scheduling";
+import { ScheduleNotFoundException } from "../exceptions/domain.exceptions";
+import { SchedulingStatusVO } from "../value-objects/status-scheduling";
+
+
+export class ChangeStatusDomainService {
+  static changeStatus(
+    schedulings: Scheduling[],
+    id: string,
+    newStatus: SchedulingStatusVO
+  ): Scheduling {
+    const scheduling = schedulings.find(a => a.id === id);
+
+    if (!scheduling) {
+      throw new ScheduleNotFoundException(id);
+    }
+    scheduling.changeStatus(newStatus);
+
+    return scheduling;
+  }
+}
